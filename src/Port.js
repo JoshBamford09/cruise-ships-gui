9f4818d09f634d6333ea23699f965ab78a2b2fbd
Josh Bamford <joshbamford09@gmail.com>
2022-11-10 21:02:00 +0000

    made objecst browser friendly and still pass tests

--- src/Port.js
+++ src/Port.js
@@ -1,16 +1,22 @@
-const Ship = require("./Ship");
-
-function Port(name){
+(function exportPort() {
+    function Port(name){
     this.name = name;
     this.ships = []
-};
+    };
 
-Port.prototype.addShip = function(ship) {
-    this.ships.push(ship);
-};
+    Port.prototype = {
+        addShip(ship) {
+          this.ships.push(ship);
+        },
+        removeShip(ship) {
+          this.ships = this.ships.filter(dockedShip => dockedShip !== ship);
+        },
+    };
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Port;
+    } else {
+        window.Port = Port;
+    }
+}());
 
-Port.prototype.removeShip = function(ship) {
-    this.ships.pop(ship);
-};
 
-module.exports = Port;
