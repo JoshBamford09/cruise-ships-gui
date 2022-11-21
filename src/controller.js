a2228d0738dba7dfedd528b4237631f4923b9f69
Josh Bamford <joshbamford09@gmail.com>
2022-11-21 19:53:39 +0000

    added ports to GUI, RenderPorts function added

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,10 @@
 (function exportController() {
     function Controller() {
         this.initialiseSea()
-    }
+}
 
-    Controller.prototype.initialiseSea = function initialiseSea() {
+Controller.prototype = {
+    initialiseSea() {
         const backgrounds = [
             './images/water0.png',
             './images/water1.png',
@@ -13,10 +14,24 @@
             document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
         backgroundIndex += 1;
         }, 1000);
-    };
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = Controller;
-    } else {
-        window.Controller = Controller;
+    },
+    renderPorts(ports) {
+        const portsElement = document.querySelector('#ports');
+        portsElement.style.width = '0px';
+        ports.forEach((port, index) => {
+           const newPortElement = document.createElement('div');
+           newPortElement.className = 'port';
+           newPortElement.dataset.portName = port.name;
+           newPortElement.dataset.portIndex = index;
+           portsElement.appendChild(newPortElement);
+           const portsElementWidth = parseInt(portsElement.style.width, 10);
+           portsElement.style.width = `${portsElementWidth + 256}px`;
+        });
     }
+};
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Controller;
+} else {
+    window.Controller = Controller;
+}
 }());
